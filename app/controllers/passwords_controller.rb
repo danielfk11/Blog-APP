184e4e64a413ac0d118cedb90515fa4603eba5d0
danielfk11 <danielfkiffer@outlook.com>
2024-04-16 21:02:51 -0300

    Add func Reset password via token SMS
    Adicionado funcao para resetar via token SMS

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,18 +1,71 @@
+# app/controllers/passwords_controller.rb
+
 class PasswordsController < ApplicationController
-    def new
-    end
+  def new
+  end
+
+  def create
+    user = User.find_by(phone_number: params[:phone_number])
+    if user
+      # Gerar e salvar token
+      token = rand(100000..999999).to_s
+      user.update(reset_password_token: token, reset_password_sent_at: Time.now)
   
-    def create
-      user = User.find_by(email: params[:email])
-      if user
-        # Implementar envio de email com instruções para resetar senha
-        redirect_to root_path, notice: 'Email enviado com instruções para resetar senha'
-      else
-        flash.now[:alert] = 'Email não encontrado'
-        render :new
-      end
-    end
+      # Enviar token via SMS
+      send_sms(user.phone_number, "Seu código de verificação é: #{token}")
   
-    # Implementar métodos edit e update para resetar a senha
+      redirect_to verify_token_path(email: user.email), notice: 'Token enviado com sucesso'
+    else
+      flash.now[:alert] = 'Número de telefone não encontrado'
+      render :new
+    end
   end
   
+
+  def edit
+    @user = User.find_by(reset_password_token: params[:reset_password_token])
+    if @user.nil?
+      redirect_to root_path, alert: 'Token inválido ou expirado'
+    end
+  end
+  
+  def update
+    @user = User.find_by(email: params[:user][:email])
+    
+    if @user&.update(user_params)
+      bypass_sign_in(@user)  # Adicione esta linha para autenticar o usuário
+      redirect_to root_path, notice: 'Senha atualizada com sucesso!'
+    else
+      render :edit
+    end
+  end
+  
+  
+  def verify_token
+  end
+  
+  def validate_token
+    user = User.find_by(email: params[:email])
+    
+    if user && user.reset_password_token == params[:token]
+      redirect_to edit_password_path(reset_password_token: user.reset_password_token), notice: 'Token validado com sucesso'
+    else
+      flash.now[:alert] = 'Token inválido'
+      render :verify_token
+    end
+  end  
+
+  private
+
+  def send_sms(to, body)
+    Twilio::REST::Client.new.messages.create(
+      to: to,
+      from: '+12563056344', # Twilio celular 
+      body: body
+    )
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'custom_sessions'
+    sessions: 'custom_sessions',
+    passwords: 'passwords'
   }
 
   get "up" => "rails/health#show", as: :rails_health_check
@@ -24,7 +25,11 @@ Rails.application.routes.draw do
 
   get '/forgot_password', to: 'passwords#new'
   post '/forgot_password', to: 'passwords#create'
-  get '/reset_password', to: 'passwords#edit'
+  get '/verify_token', to: 'passwords#verify_token'
+  post '/validate_token', to: 'passwords#validate_token'
+  get '/edit_password', to: 'passwords#edit', as: 'edit_password'
+  get '/new_password', to: 'passwords#new'
+  post '/reset_password', to: 'passwords#reset_password'
   patch '/reset_password', to: 'passwords#update'
 
   # Logado:
