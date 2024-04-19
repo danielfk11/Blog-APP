370d82f2e2cc7caad189f35a80c60275d5c5f3f5
danielfk11 <danielfkiffer@outlook.com>
2024-04-19 09:54:46 -0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,6 @@ class PostsController < ApplicationController
       @posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
     end
   end
-  
 
   def show
   end
@@ -22,6 +21,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
+      assign_tags_to_post
       redirect_to @post, notice: 'Post criado com sucesso!'
     else
       render :new
@@ -34,11 +34,11 @@ class PostsController < ApplicationController
       redirect_to posts_path, alert: 'Você não tem permissão para editar este post.'
     end
   end
-  
 
   def update
     @post = current_user.posts.find(params[:id])
     if @post.update(post_params)
+      assign_tags_to_post
       redirect_to @post, notice: 'Post atualizado com sucesso!'
     else
       render :edit
@@ -46,7 +46,6 @@ class PostsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     redirect_to posts_path, alert: 'Você não tem permissão para editar este post.'
   end
-  
 
   def destroy
     if @post.user == current_user
@@ -58,6 +57,15 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def assign_tags_to_post
+    return unless params[:post][:tag_names].present?
+
+    tag_names = params[:post][:tag_names].split(',')
+    @post.tags = tag_names.map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
 
   def set_post
     @post = Post.find(params[:id])
